78486cf3747c9f66f65105c6c369c69a2a99e9bd
OtbeyNat <72035811+OtbeyNat@users.noreply.github.com>
2024-07-03 15:53:10 -0400

    homescreenupdate

---
+++ components/Trending.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import {
+    FlatList,
+    Image,
+    ImageBackground,
+    Text,
+    TouchableOpacity,
+  } from "react-native";
+import { icons } from "../constants";
+
+const zoomIn = {
+  0: {
+    scale: 0.9,
+  },
+  1: {
+    scale: 1,
+  },
+};
+
+const zoomOut = {
+  0: {
+    scale: 1,
+  },
+  1: {
+    scale: 0.9,
+  },
+};
+
+const Trending = ({ posts }) => {
+  return (
+    <FlatList
+        data={posts}
+        horizontal
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+            <Text className="text-3xl text-white">{item.id}</Text>
+        )}
+    
+    />
+  )
+}
+
+export default Trending
